bb5a651cd6beb7d609f4007bb935a0491cab1881
yaroslavdrapei <yarikdrapey@gmail.com>
2024-04-08 21:25:09 +0300

    added: visualization for matrixes

--- Visualization.py
+++ Visualization.py
@@ -8,7 +8,7 @@ class Visualization:
     # letters is easier to look at than numbers so table will look like - {0: 'A', 1: 'B', ..., 25: 'C'}
     table: dict[int, str] = {idx: letter for idx, letter in enumerate(letters)}
     @staticmethod
-    def visualize(graph: Graph):
+    def visualize_graph(graph: Graph) -> None:
         nx_graph = nx.Graph(np.array(graph.get_matrix()))
         labels = None
         if graph.get_number() <= 25:
@@ -17,3 +17,7 @@ class Visualization:
         nx.draw(nx_graph, with_labels=True, labels=labels)
 
         plt.show()
+    
+    def visualize_matrix(matrix: list[list[int]]) -> None:
+        for row in matrix:
+            print(row)
