e302228c075c8c5fd71810c3767eee2d1110cde0
yaroslavdrapei <yarikdrapey@gmail.com>
2024-04-09 21:46:14 +0300

    added n_of_edges property

--- Graph.py
+++ Graph.py
@@ -1,7 +1,18 @@
 class Graph:
     def __init__(self, matrix: list[list[int]]):
         self.__matrix = matrix # adjacency matrix (матриця суміжності)
+        self.__n_of_edges = 0
+        self.__calc_edges()
     
+    def __calc_edges(self):
+        for i in range(self.get_number()):
+            for j in range(self.get_number()):
+                if j > i:
+                    self.__n_of_edges += self.__matrix[i][j]
+
+    def get_n_of_edges(self):
+        return self.__n_of_edges
+
     def get_matrix(self):
         return self.__matrix
     
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from Graph import *
 from GraphGenerator import *
 from WarshallAlgorithm import *
 
-graph = GraphGenerator.generate(20, 10)
+graph = GraphGenerator.generate(7, 50)
+
+print(graph.get_n_of_edges())
 
 reachability_matrix = WarshallAlgorithm.get_reachability_matrix(graph)
 
