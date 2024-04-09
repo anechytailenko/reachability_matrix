7b47af03b10dfd91097b8086b61d06964a37193d
Yaroslav Drapey <91949818+yaroslavdrapei@users.noreply.github.com>
2024-04-09 21:57:46 +0300

    Edges and n of edges property + visualization labels on/off (#7)
    * added n_of_edges property
    
    * collecting all edges; improved visualization

--- Graph.py
+++ Graph.py
@@ -1,7 +1,21 @@
 class Graph:
     def __init__(self, matrix: list[list[int]]):
         self.__matrix = matrix # adjacency matrix (матриця суміжності)
+        self.__edges: list[tuple[int]] = []
+        self.__calc_edges()
     
+    def __calc_edges(self):
+        for i in range(self.get_number()):
+            for j in range(self.get_number()):
+                if j > i and self.__matrix[i][j] == 1:
+                    self.__edges.append((i, j))
+
+    def get_n_of_edges(self):
+        return len(self.__edges)
+
+    def get_edges(self):
+        return self.__edges
+
     def get_matrix(self):
         return self.__matrix
     
--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from Graph import *
 from GraphGenerator import *
 from WarshallAlgorithm import *
 
-graph = GraphGenerator.generate(20, 10)
+graph = GraphGenerator.generate(7, 50)
+
+print(graph.get_n_of_edges())
 
 reachability_matrix = WarshallAlgorithm.get_reachability_matrix(graph)
 
-Visualization.visualize_matrix(reachability_matrix, labels=True)
+# Visualization.visualize_matrix(reachability_matrix, labels=True)
 
-Visualization.visualize_graph(graph)
+print(graph.get_edges())
+
+Visualization.visualize_graph(graph, labels=False)
